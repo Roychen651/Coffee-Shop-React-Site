af5f9f83407427fa49969158d4caabf5d460a16b
Roy Chen <roy97chen@gmail.com>
2024-07-30 15:49:12 +0300

    finished

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 import HeroImg from "../../assets/coffee2.png";
+
 const Home = () => {
   return (
-    <div className="min-h-[550px] sm:min-h[600px]
-     bg-brandDark flex justify-center items-center text-white">
+    <div className="min-h-[550px] sm:min-h-[600px] bg-brandDark flex justify-center items-center text-white">
       <div className="container pb-8 sm:pb-0">
         <div className="grid grid-cols-1 sm:grid-cols-2">
-            {/* text content section */}
+          {/* text content section */}
+          <div
+            className="order-2 sm:order-1 flex flex-col justify-center gap-6"
+            data-aos="fade-up"
+            data-aos-once="true"
+          >
+            <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+              We sell the richest
+              <span
+                className="text-primary font-cursive"
+                data-aos="zoom-out"
+                data-aos-delay="300"
+              >
+                {" "}
+                Coffee Beans{" "}
+              </span>
+              in the country
+            </h1>
+          </div>
 
-            <div className="order-2 sm:order-1 flex flex-col justify-center gap-6">
-                <h1 className="text-5xl sm:text-6xl lg:text-7xl
-                font-bold">
-                    We sell the richest
-                    <span className="text-primary font-cursive"> Coffee Beans </span>
-                     in the country
-                </h1>
-                
-            </div>
-                
-            {/* Image section */}
-            <div className="min-h-[450px] flex justify-center items-center order-1 sm:order-2 relative">
-                <img src={HeroImg} alt=""
-                className="w-[300px] sm:w-[450px] sm:scale-110 mx-auto
-                spin "  />
-            </div>
+          {/* Image section */}
+          <div
+            className="min-h-[450px] flex justify-center items-center order-1 sm:order-2 relative"
+            data-aos="zoom-in"
+            data-aos-duration="300"
+          >
+            <img
+              src={HeroImg}
+              alt=""
+              className="w-[300px] sm:w-[450px] sm:scale-110 mx-auto spin"
+              data-aos-once="true"
+            />
+          </div>
         </div>
       </div>
     </div>
